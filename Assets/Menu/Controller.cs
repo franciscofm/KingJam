247629b4e2b50e7630ef42028946181b584bc04e
Fran <francisco.fernandez.manyas@gmail.com>
2018-06-09 13:09:16 +0200

    V test

--- Assets/Menu/Controller.cs
+++ Assets/Menu/Controller.cs
@@ -7,7 +7,9 @@ namespace Menu {
 	public class Controller : MonoBehaviour {
 
 		public void ShowMenu() {
-
+			float rot = transform.localRotation.eulerAngles.z;
+			rot = Mathf.Clamp (rot, -20, 20);
+			transform.rotation.eulerAngles = new Vector3(0f,0f,rot);
 		}
 
 		public void StartGame() {
