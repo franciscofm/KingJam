b4debe3143ef1d5b2ff83d7cd07340dde08a5307
VitaminaCPP <pau.montequi@gmail.com>
2018-06-09 13:49:49 +0200

    save escene

--- Assets/Menu/Controller.cs
+++ Assets/Menu/Controller.cs
@@ -9,7 +9,7 @@ namespace Menu {
 		public void ShowMenu() {
 			float rot = transform.localRotation.eulerAngles.z;
 			rot = Mathf.Clamp (rot, -20, 20);
-			transform.rotation.eulerAngles = new Vector3(0f,0f,rot);
+			//transform.rotation.eulerAngles = new Vector3(0f,0f,rot);
 		}
 
 		public void StartGame() {
