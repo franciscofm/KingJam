440bc3ecf2829157b99cd1359f4b750133baa7fb
VitaminaCPP <pau.montequi@gmail.com>
2018-06-09 13:11:23 +0200

    Ein kilo kartoffen bite

---
+++ Assets/Game/ClampBaseRotation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClampBaseRotation : MonoBehaviour {
+
+    static float maxRotation = 20f;
+    static float midRotation = maxRotation + (360f - maxRotation * 2f) / 2f;
+
+    public AnimationCurve clampCurve;
+    public Rigidbody body;
+
+    void Update () {
+        float rotation = transform.eulerAngles.z;
+        Vector3 r = new Vector3(0f, 0f, rotation);
+        if (rotation > maxRotation)
+        {
+            if (rotation < midRotation)
+            {
+                float diff = rotation / maxRotation;
+                r.z = maxRotation;
+                body.angularVelocity = Vector3.zero * (1f - clampCurve.Evaluate(diff));
+            }
+            else if (rotation < 360 - maxRotation)
+            {
+                float diff = rotation / midRotation;
+                r.z = 360f - maxRotation;
+                body.angularVelocity = Vector3.zero * (1f - clampCurve.Evaluate(diff));
+            }
+        }
+        transform.eulerAngles = r;
+    }
+}
--- Assets/Game/Player.cs
+++ Assets/Game/Player.cs
@@ -16,7 +16,6 @@ namespace Game {
 		// Update is called once per frame
 		void Update () {
 			Vector3 dir = new Vector3 (Input.GetAxis ("Horizontal"), 0f, Input.GetAxis ("Vertical"));
-			Debug.Log (dir);
 			body.AddForce (speed * dir);
 		}
 	}
