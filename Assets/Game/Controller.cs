1aaddf8697c6fe49811fe0e7fc3c8c3625fc1e09
Fran <francisco.fernandez.manyas@gmail.com>
2018-06-09 18:23:45 +0200

    Game.Controller LevelUp stuff

--- Assets/Game/Controller.cs
+++ Assets/Game/Controller.cs
@@ -9,19 +9,55 @@ namespace Game {
 		public Transform playT;
 		public Transform spawnT;
 		public Transform floorT;
-        public Pipe pipeS;
+		public Transform pipeT;
 		public static Controller instance;
 
-		Player player;
-		List<Enemy> enemies;
+
+		public float[] TimeLevels = new float[]{5f, 10f, 20f};
+		public float gameTime;
+		public bool finished;
+
+		Player playerS;
+		List<Enemy> enemiesS;
+		Pipe pipeS;
+		//Floor floorS;
+		int level;
 
 		void Awake() {
 			if (instance != null) Destroy (instance.gameObject);
 			instance = this;
             FixSpriteRotation.floor = floorT;
         }
+
 		void Start() {
+			playerS = playT.GetComponent<Player> ();
+			pipeS = pipeT.GetComponent<Pipe> ();
+			//floorS = floorT.GetComponent<Floor> ();
+
+			gameTime = 0f;
+			level = 0;
+			finished = false;
 			pipeS.StartGame ();
+		}
+		void Update() {
+			gameTime += Time.deltaTime;
+			if (!finished) {
+				if (gameTime > TimeLevels [level])
+					LevelUp ();
+
+				++level;
+				if(level >= TimeLevels.Length) {
+					finished = true;
+					return;
+				}
+			}
+		}
+		void LevelUp() {
+			//playerS.LevelUp ();
+			//for (int i = 0; i < enemiesS.Count; ++i)
+			//	enemiesS [i].LevelUp ();
+			//pipeS.LevelUp();
+			//floorS.LevelUp();
 		}
 	}
 }
