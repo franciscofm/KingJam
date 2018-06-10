eedd8cda061d70339b97b39a0d0fe3837113ddb5
VitaminaCPP <pau.montequi@gmail.com>
2018-06-10 15:16:50 +0200

    sonido

--- Assets/Game/Player.cs
+++ Assets/Game/Player.cs
@@ -9,12 +9,14 @@ namespace Game {
         int currentLevel = 0;
 
 		public Animator animator;
+        AudioSource corre;
 
         Vector3 force = Vector3.zero;
 		public float speed;
 		// Use this for initialization
 		void Start () {
 			if (body == null) body = GetComponent<Rigidbody> ();
+            corre = GetComponent<AudioSource>();
 		}
 		
 		// Update is called once per frame
@@ -24,10 +26,12 @@ namespace Game {
             if (Mathf.Abs(force.magnitude) > 0)
             {
                 animator.Play("walk_" + currentLevel);
+                corre.Play();
             }
             else
             {
                 animator.Play("idle_" + currentLevel);
+                corre.Stop();
             }
         }
 
